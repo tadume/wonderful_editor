b21581f4668c869fa78e8fffc91d96f3f4bb5ff9
田爪大智 <>
2022-10-15 16:59:05 +0900

    記事作成機能の実装

--- spec/requests/api/v1/article_spec.rb
+++ spec/requests/api/v1/article_spec.rb
@@ -49,4 +49,19 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
   end
+
+  describe "POST /articles/:id" do
+    subject { post(api_v1_articles_path, params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    it "記事のレコードが作成される", :aggregate_failures do
+      expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
+      res = JSON.parse(response.body)
+      expect(res["title"]).to eq params[:article][:title]
+      expect(res["body"]).to eq params[:article][:body]
+    end
+  end
 end
